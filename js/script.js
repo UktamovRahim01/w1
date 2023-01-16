4c05a92e0b62c315d289f86a11499ccc129e2524
UktamovRahim01 <121957549+UktamovRahim01@users.noreply.github.com>
2023-01-16 17:06:55 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+let name = "Djon";
+let admin = "aa";
+admin = name;
+
+console.log(admin);
+
+
+let balance = 25000;
+let car = 4480;
+let food = 890 ;
+let invoice =5500;
+let stok = 4200;
+let itog = "Итог ";
+let totalBalance = 0;
+
+
+let trati = car + food;
+let dohod = invoice + stok;
+totalBalance = balance - trati + dohod;
+
+console.log(itog + totalBalance);
+
+
+
